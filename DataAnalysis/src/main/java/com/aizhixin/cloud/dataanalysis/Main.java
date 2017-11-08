e3675732e59057a1a66b54cac5b1ded27ddb507f
zhengning <ning.zheng@dinglicom.com>
2017-11-08 16:47:52 +0800

    master

---
+++ DataAnalysis/src/main/java/com/aizhixin/cloud/dataanalysis/Main.java
@@ -0,0 +1,14 @@
+package com.aizhixin.cloud.dataanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+@SpringCloudApplication
+public class Main {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+}
