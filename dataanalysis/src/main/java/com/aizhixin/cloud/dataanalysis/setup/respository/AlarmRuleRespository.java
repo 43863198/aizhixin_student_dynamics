433326b3b6c66c201b539d2faaf8f73d49e307c2
zhengning <ning.zheng@dinglicom.com>
2017-11-26 10:32:52 +0800

    提交

--- dataanalysis/src/main/java/com/aizhixin/cloud/dataanalysis/setup/respository/AlarmRuleRespository.java
+++ dataanalysis/src/main/java/com/aizhixin/cloud/dataanalysis/setup/respository/AlarmRuleRespository.java
@@ -2,12 +2,13 @@ package com.aizhixin.cloud.dataanalysis.setup.respository;
 
 
 import java.util.List;
-
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
-
+import org.springframework.data.repository.query.Param;
 import com.aizhixin.cloud.dataanalysis.setup.entity.AlarmRule;
 
 public interface AlarmRuleRespository extends PagingAndSortingRepository<AlarmRule, String> {
 	
-//	List<AlarmRule> findAllByDeleteFlagAndWarningType(int deleteFlag,String warningType);
+	@Query("select ar from com.aizhixin.cloud.dataanalysis.setup.entity.AlarmRule ar where ar.id in (:ids) ")
+	List<AlarmRule> findAllByIds(@Param("ids") String[] ids);
 }
