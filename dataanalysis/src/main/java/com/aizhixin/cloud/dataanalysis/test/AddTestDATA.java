865bd725c1842cc6e5929ba388598356ea87a014
wujianwei <wujianwei@aizhixin.com>
2017-11-27 20:24:29 +0800

    api 提交

--- dataanalysis/src/main/java/com/aizhixin/cloud/dataanalysis/test/AddTestDATA.java
+++ dataanalysis/src/main/java/com/aizhixin/cloud/dataanalysis/test/AddTestDATA.java
@@ -21,7 +21,7 @@ public class AddTestDATA {
     private WarningTypeService warningTypeService;
 
 
-//    @Scheduled(cron = "0 0/10 * * * ?")
+//    @Scheduled(cron = "0 0/3 * * * ?")
     public  void setWarningType(){
             Long orgId = 213L;
 		    List<WarningType> warningTypeList = new ArrayList<>();
