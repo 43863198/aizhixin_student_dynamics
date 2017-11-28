01cd9271d3be45a985554a570db8fb0aa274f71b
wujianwei <wujianwei@aizhixin.com>
2017-11-28 19:33:22 +0800

    api 提交

--- dataanalysis/src/main/java/com/aizhixin/cloud/dataanalysis/alertinformation/dto/WarningGradeDTO.java
+++ dataanalysis/src/main/java/com/aizhixin/cloud/dataanalysis/alertinformation/dto/WarningGradeDTO.java
@@ -3,7 +3,6 @@ package com.aizhixin.cloud.dataanalysis.alertinformation.dto;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
-import com.aizhixin.cloud.dataanalysis.alertinformation.dto.WaringDescParameterDTO;
 import java.util.List;
 
 /**
@@ -23,6 +22,6 @@ public class WarningGradeDTO {
     @ApiModelProperty(value = "开启状态(10:启用 ;20:关闭；")
     private int setupCloseFlag;
     @ApiModelProperty(value = "规则描述及参数")
-    List<WaringDescParameterDTO> describeParameter;
+    List<WarningDescParameterDTO> describeParameter;
 
 }
