874768de3442fbd28870d648bf7154db7b708c36
zhengning <ning.zheng@dinglicom.com>
2017-11-30 09:55:58 +0800

    提交

--- dataanalysis/src/main/java/com/aizhixin/cloud/dataanalysis/studentRegister/controller/TestDataController.java
+++ dataanalysis/src/main/java/com/aizhixin/cloud/dataanalysis/studentRegister/controller/TestDataController.java
@@ -63,15 +63,10 @@ public class TestDataController {
 	@Autowired
 	private RollCallMongoRespository rollCallMongoRespository;
 	@Autowired
-	private RollCallCountMongoRespository rollCallCountMongoRespository;
-	@Autowired
 	private ScoreMongoRespository scoreMongoRespository;
 	@Autowired
-	private StudentRegisterJob studentRegisterJob;
-	@Autowired
-	private RollCallJob rollCallJob;
-	@Autowired
 	private ScoreJob scoreJob;
+	@Autowired
 	private StudentRegisterService registerService;
 	@Autowired
 	private ScoreService scoreService;
@@ -251,95 +246,6 @@ public class TestDataController {
 		return new ResponseEntity<Map<String, Object>>(result, HttpStatus.OK);
 	}
 
-	@RequestMapping(value = "/rollcallcountjob", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	@ApiOperation(httpMethod = "GET", value = "签到定统计数据时任务", response = Void.class, notes = "签到定统计数据时任务<br><br><b>@author zhengning</b>")
-	public ResponseEntity<Map<String, Object>> rollCallCountJob() {
-
-		rollCallJob.rollCallCountJob();
-
-		Map<String, Object> result = new HashMap<String, Object>();
-		return new ResponseEntity<Map<String, Object>>(result, HttpStatus.OK);
-	}
-
-	@RequestMapping(value = "/rollcalljob", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	@ApiOperation(httpMethod = "GET", value = "签到预警定时任务", response = Void.class, notes = "签到预警定时任务<br><br><b>@author zhengning</b>")
-	public ResponseEntity<Map<String, Object>> rollCallJob() {
-
-		rollCallJob.rollCallJob();
-
-		Map<String, Object> result = new HashMap<String, Object>();
-		return new ResponseEntity<Map<String, Object>>(result, HttpStatus.OK);
-	}
-
-	@RequestMapping(value = "/sturegisterjob", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	@ApiOperation(httpMethod = "GET", value = "新生报到预警定时任务", response = Void.class, notes = "新生报到预警定时任务<br><br><b>@author zhengning</b>")
-	public ResponseEntity<Map<String, Object>> stuRegisterJob() {
-
-		studentRegisterJob.studenteRegisterJob();
-
-		Map<String, Object> result = new HashMap<String, Object>();
-		return new ResponseEntity<Map<String, Object>>(result, HttpStatus.OK);
-	}
-
-	@RequestMapping(value = "/totalscorecountjob", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	@ApiOperation(httpMethod = "GET", value = "总评成绩统计定时任务", response = Void.class, notes = "新生报到预警定时任务<br><br><b>@author zhengning</b>")
-	public ResponseEntity<Map<String, Object>> totalScoreCountJob() {
-		scoreJob.totalScoreCountJob();
-
-		Map<String, Object> result = new HashMap<String, Object>();
-		return new ResponseEntity<Map<String, Object>>(result, HttpStatus.OK);
-	}
-
-	@RequestMapping(value = "/totalscorejob", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	@ApiOperation(httpMethod = "GET", value = "总评成绩预警定时任务", response = Void.class, notes = "总评成绩预警定时任务<br><br><b>@author zhengning</b>")
-	public ResponseEntity<Map<String, Object>> totalScoreJob() {
-		scoreJob.totalScoreJob();
-		Map<String, Object> result = new HashMap<String, Object>();
-		return new ResponseEntity<Map<String, Object>>(result, HttpStatus.OK);
-	}
-
-	@RequestMapping(value = "/makeupscorecountjob", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	@ApiOperation(httpMethod = "GET", value = "补考成绩统计定时任务", response = Void.class, notes = "补考成绩统计定时任务<br><br><b>@author zhengning</b>")
-	public ResponseEntity<Map<String, Object>> makeUpScoreCountJob() {
-		scoreJob.makeUpScoreCountJob();
-		;
-		Map<String, Object> result = new HashMap<String, Object>();
-		return new ResponseEntity<Map<String, Object>>(result, HttpStatus.OK);
-	}
-
-	@RequestMapping(value = "/makeupscorejob", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	@ApiOperation(httpMethod = "GET", value = "补考成绩预警定时任务", response = Void.class, notes = "补考成绩预警定时任务<br><br><b>@author zhengning</b>")
-	public ResponseEntity<Map<String, Object>> makeUpScoreJob() {
-		scoreJob.makeUpScoreJob();
-		Map<String, Object> result = new HashMap<String, Object>();
-		return new ResponseEntity<Map<String, Object>>(result, HttpStatus.OK);
-	}
-
-	@RequestMapping(value = "/scorefluctuatecountJob", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	@ApiOperation(httpMethod = "GET", value = "成绩波动数据统计定时任务", response = Void.class, notes = "成绩波动数据统计定时任务<br><br><b>@author zhengning</b>")
-	public ResponseEntity<Map<String, Object>> scoreFluctuateCountJob() {
-		scoreJob.scoreFluctuateCountJob();
-		Map<String, Object> result = new HashMap<String, Object>();
-		return new ResponseEntity<Map<String, Object>>(result, HttpStatus.OK);
-	}
-
-	@RequestMapping(value = "/scorefluctuatejob", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	@ApiOperation(httpMethod = "GET", value = "成绩波动预警定时任务", response = Void.class, notes = "成绩波动预警定时任务<br><br><b>@author zhengning</b>")
-	public ResponseEntity<Map<String, Object>> scoreFluctuateJob() {
-		scoreJob.scoreFluctuateJob();
-		Map<String, Object> result = new HashMap<String, Object>();
-		return new ResponseEntity<Map<String, Object>>(result, HttpStatus.OK);
-	}
-
-	@RequestMapping(value = "/attendabnormaljob", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	@ApiOperation(httpMethod = "GET", value = "修学异常预警定时任务", response = Void.class, notes = "总评成绩统计定时任务执行后,执行修学异常预警定时任务<br><br><b>@author zhengning</b>")
-	public ResponseEntity<Map<String, Object>> attendAbnormalJob() {
-		scoreJob.attendAbnormalJob();
-		Map<String, Object> result = new HashMap<String, Object>();
-		return new ResponseEntity<Map<String, Object>>(result, HttpStatus.OK);
-	}
-
-	
 	@RequestMapping(value = "/importStudent", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
 	@ApiOperation(httpMethod = "POST", value = "批量导入新生报到信息", response = Void.class, notes = "批量导入新生报到信息<br><br><b>@author bly</b>")
 	public ResponseEntity<Void> importStudent(
