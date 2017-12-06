8a969cd243b5a51b259b1a5ccaf959316580d584
bly <bailiuyi@aizhixin.com>
2017-12-06 10:07:45 +0800

    修改文件路径

--- dataanalysis/src/main/java/com/aizhixin/cloud/dataanalysis/common/excelutil/ExcelUtil.java
+++ dataanalysis/src/main/java/com/aizhixin/cloud/dataanalysis/common/excelutil/ExcelUtil.java
@@ -1,7 +1,9 @@
 package com.aizhixin.cloud.dataanalysis.common.excelutil;
 
-import lombok.Getter;
-import lombok.Setter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -11,45 +13,51 @@ import org.springframework.web.multipart.MultipartFile;
 import com.aizhixin.cloud.dataanalysis.common.exception.CommonException;
 import com.aizhixin.cloud.dataanalysis.common.exception.ErrorCode;
 
-import java.io.InputStream;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * Created by zhen.pan on 2017/7/14.
  */
 public class ExcelUtil {
-    private Workbook wb;
-    @Getter @Setter private boolean isE2007 = false; // 判断是否是excel2007格式
-    public ExcelUtil(MultipartFile file) {
-        String path = file.getOriginalFilename();// 返回原始文件名
-        String extName = null;
-        int p = path.lastIndexOf(".");
-        if (p >= 0) {
-            extName = path.substring(p + 1);
-        }
-        isE2007 = "xlsx".equalsIgnoreCase(extName) ? true : false;
-        InputStream input;
-        try {
-            input = file.getInputStream();
-            if (isE2007) {
-                wb = new XSSFWorkbook(input);
-            } else {
-                wb = new HSSFWorkbook(input);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new CommonException(ErrorCode.SYSTEM_EXCEPTION_CODE, e.getMessage());
-        }
-    }
+	private Workbook wb;
+	@Getter
+	@Setter
+	private boolean isE2007 = false; // 判断是否是excel2007格式
 
-    public Sheet getSheet(String sheetName) {
-        return wb.getSheet(sheetName);
-    }
+	public ExcelUtil(MultipartFile file) {
+		File excelFile = new File("\\nfs\\1");
+		String path = excelFile.getName();// 返回原始文件名
+		String extName = null;
+		int p = path.lastIndexOf(".");
+		if (p >= 0) {
+			extName = path.substring(p + 1);
+		}
+		isE2007 = "xlsx".equalsIgnoreCase(extName) ? true : false;
+		InputStream input;
+		try {
+			// input = excelFile.getInputStream();
+			input = new FileInputStream(excelFile);
+			if (isE2007) {
+				wb = new XSSFWorkbook(input);
+			} else {
+				wb = new HSSFWorkbook(input);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new CommonException(ErrorCode.SYSTEM_EXCEPTION_CODE, e.getMessage());
+		}
+	}
 
-    public Sheet getSheet(int sheetIndex) {
-        return wb.getSheetAt(sheetIndex);
-    }
-    
-    public int getSheetNumber() {
-    	return wb.getNumberOfSheets();
-    }
+	public Sheet getSheet(String sheetName) {
+		return wb.getSheet(sheetName);
+	}
+
+	public Sheet getSheet(int sheetIndex) {
+		return wb.getSheetAt(sheetIndex);
+	}
+
+	public int getSheetNumber() {
+		return wb.getNumberOfSheets();
+	}
 }
