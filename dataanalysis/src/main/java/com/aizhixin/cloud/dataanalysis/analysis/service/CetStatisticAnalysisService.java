7cba51ba1a23141b01e7a3db5c22d58f6e967343
wujianwei <wujianwei@aizhixin.com>
2017-12-08 16:14:36 +0800

    api 提交

--- dataanalysis/src/main/java/com/aizhixin/cloud/dataanalysis/analysis/service/CetStatisticAnalysisService.java
+++ dataanalysis/src/main/java/com/aizhixin/cloud/dataanalysis/analysis/service/CetStatisticAnalysisService.java
@@ -3,20 +3,24 @@ package com.aizhixin.cloud.dataanalysis.analysis.service;
 import com.aizhixin.cloud.dataanalysis.alertinformation.dto.CollegeWarningInfoDTO;
 import com.aizhixin.cloud.dataanalysis.analysis.dto.CetStatisticDTO;
 import com.aizhixin.cloud.dataanalysis.analysis.dto.CollegeCetStatisticDTO;
+import com.aizhixin.cloud.dataanalysis.analysis.dto.TrendDTO;
 import com.aizhixin.cloud.dataanalysis.analysis.entity.SchoolStatistics;
 import com.aizhixin.cloud.dataanalysis.common.PageData;
 import com.aizhixin.cloud.dataanalysis.common.util.ProportionUtil;
+import com.aizhixin.cloud.dataanalysis.score.mongoEntity.Score;
+import com.aizhixin.cloud.dataanalysis.studentRegister.mongoEntity.StudentRegister;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author: Created by jianwei.wu
@@ -29,6 +33,8 @@ public class CetStatisticAnalysisService {
 
     @Autowired
     private EntityManager em;
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
     public Map<String,Object> getStatistic(Long orgId, String grade, Integer semester, Pageable pageable){
         Map<String,Object> result = new HashMap<>();
@@ -146,6 +152,146 @@ public class CetStatisticAnalysisService {
         return result;
     }
 
+
+    public Map<String,Object> getCetTrendAnalysis(Long orgId, Long collegeId, Integer type){
+        Map<String,Object> result = new HashMap<>();
+        List<TrendDTO> trendDTOList = new ArrayList<>();
+        Map<String, Object> condition = new HashMap<>();
+        try {
+            StringBuilder sql = new StringBuilder("SELECT SUM(cet.CET_FORE_JOIN_NUM) as sumc4, SUM(cet.CET_FORE_PASS_NUM) as sump4, SUM(cet.CET_SIX_JOIN_NUM) as sumc6, SUM(cet.CET_SIX_PASS_NUM) as sump6, cet.TEACHER_YEAR FROM T_CET_STATISTICS cet WHERE 1 = 1");
+            Query sq = em.createNativeQuery(sql.toString());
+            for (Map.Entry<String, Object> e : condition.entrySet()) {
+                sq.setParameter(e.getKey(), e.getValue());
+            }
+            if (null != orgId) {
+                sql.append(" and cet.ORG_ID = :orgId");
+                condition.put("orgId", orgId);
+            }
+            if (null != collegeId) {
+                sql.append(" and cet.COLLOEGE_ID = :collegeId");
+                condition.put("collegeId", collegeId);
+            }
+            sql.append(" group by cet.TEACHER_YEAR");
+            for (Map.Entry<String, Object> e : condition.entrySet()) {
+                sq.setParameter(e.getKey(), e.getValue());
+            }
+            List<Object> res = sq.getResultList();
+            int count4 = 0;
+            int count6 = 0;
+            int pass4 = 0;
+            int pass6 = 0;
+            if (type == 4) {
+                if (null != res) {
+                    for (Object obj : res) {
+                        Object[] d = (Object[]) obj;
+                        if (d.length == 5) {
+                            TrendDTO trendDTO = new TrendDTO();
+                            if (null != d[0]) {
+                                count4 = Integer.valueOf(String.valueOf(d[0]));
+                            }
+                            if (null != d[1]) {
+                                pass4 = Integer.valueOf(String.valueOf(d[1]));
+                            }
+                            if (null != d[4]) {
+                                trendDTO.setYear(String.valueOf(d[4]));
+                            }
+                            trendDTO.setValue(ProportionUtil.accuracy(pass4 * 1.0, count4 * 1.0, 2));
+                            trendDTOList.add(trendDTO);
+                        }
+                    }
+
+                }
+            }
+            if (type == 6) {
+                if (null != res) {
+                    for (Object obj : res) {
+                        Object[] d = (Object[]) obj;
+                        if (d.length == 5) {
+                            TrendDTO trendDTO = new TrendDTO();
+                            if (null != d[2]) {
+                                count6 = Integer.valueOf(String.valueOf(d[2]));
+                            }
+                            if (null != d[3]) {
+                                pass6 = Integer.valueOf(String.valueOf(d[3]));
+                            }
+                            if (null != d[4]) {
+                                trendDTO.setYear(String.valueOf(d[4]));
+                            }
+                            trendDTO.setValue(ProportionUtil.accuracy(pass6 * 1.0, count6 * 1.0, 2));
+                            trendDTOList.add(trendDTO);
+                        }
+                    }
+
+                }
+            }
+        }catch (Exception e){
+            result.put("success",false);
+            result.put("message","获取统计分析数据失败！");
+            return result;
+        }
+        result.put("success",true);
+        result.put("data",trendDTOList);
+        return result;
+    }
+
+
+    public Map<String, Object> getCetDetail(Long orgId, String collegeId, String grade, Integer type, Pageable page) {
+        Map<String, Object> result = new HashMap<>();
+        PageData<Score> p = new PageData<>();
+        List<Score> items = new ArrayList<>();
+        long total = 0L;
+        try {
+            //创建排序模板Sort
+            Sort sort = new Sort(Sort.Direction.DESC, "id");
+            //创建分页模板Pageable
+            Pageable pageable = new PageRequest(page.getPageNumber(), page.getPageSize(), sort);
+            //创建查询条件对象
+            org.springframework.data.mongodb.core.query.Query query = new org.springframework.data.mongodb.core.query.Query();
+            //条件
+            Criteria criteria = Criteria.where("orgId").is(orgId);
+            if (null != collegeId) {
+                String[] cid = collegeId.split(",");
+                Set<Long> collegeIds = new HashSet<>();
+                for (String d : cid) {
+                    collegeIds.add(Long.valueOf(d));
+                }
+                criteria.and("collegeId").in(collegeIds);
+            }
+            if (null != grade) {
+                String[] td = grade.split(",");
+                List<String> tds = new ArrayList<>();
+                for (String d : td) {
+                    tds.add(d);
+                }
+                criteria.and("grade").in(tds);
+            }
+            if (null != type) {
+                if(type==4) {
+                    criteria.and("examType").is("cet4");
+                }else if(type==6){
+                    criteria.and("examType").is("cet6");
+                }else {
+                    criteria.orOperator(criteria.where("examType").is("cet4"), criteria.where("examType").is("cet6"));
+                }
+            }
+            query.addCriteria(criteria);
+            //mongoTemplate.count计算总数
+            total = mongoTemplate.count(query, StudentRegister.class);
+            // mongoTemplate.find 查询结果集
+            items = mongoTemplate.find(query.with(pageable), Score.class);
+        }catch (Exception e){
+            result.put("success", false);
+            result.put("message","获取数据异常！");
+        }
+        p.getPage().setTotalPages((int)Math.ceil(total/page.getPageSize())+1);
+        p.getPage().setPageNumber(page.getPageNumber());
+        p.getPage().setPageSize(page.getPageSize());
+        p.getPage().setTotalElements(total);
+        p.setData(items);
+        result.put("success", true);
+        result.put("data", p);
+        return result;
+    }
 
 
 }
--- dataanalysis/src/main/java/com/aizhixin/cloud/dataanalysis/analysis/service/SchoolStatisticsService.java
+++ dataanalysis/src/main/java/com/aizhixin/cloud/dataanalysis/analysis/service/SchoolStatisticsService.java
@@ -10,10 +10,15 @@ import com.aizhixin.cloud.dataanalysis.analysis.respository.TeachingScoreStatist
 import com.aizhixin.cloud.dataanalysis.common.PageData;
 import com.aizhixin.cloud.dataanalysis.common.constant.DataValidity;
 import com.aizhixin.cloud.dataanalysis.common.util.ProportionUtil;
+import com.aizhixin.cloud.dataanalysis.studentRegister.mongoEntity.StudentRegister;
 import liquibase.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -40,6 +45,8 @@ public class SchoolStatisticsService {
     private CetScoreStatisticsRespository cetScoreStatisticsRespository;
     @Autowired
     private TeachingScoreStatisticsRespository teachingScoreStatisticsRespository;
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
     public Map<String, Object> getStatisticNewstudents(Long orgId, String year, Pageable pageable) {
         Map<String, Object> result = new HashMap<>();
@@ -355,5 +362,72 @@ public class SchoolStatisticsService {
         map.put("courseAndAvgScore",obj);
         map.put("collegeAndAvgScore",list);
         return map;
+    }
+
+    public Map<String, Object> getCollegeDetails(Pageable page,Long orgId, String collegeId, String nj, String type,String isReport,String isPay) {
+        Map<String, Object> result = new HashMap<>();
+        PageData<StudentRegister> p = new PageData<>();
+        List<StudentRegister> items = new ArrayList<>();
+        long total = 0L;
+        try {
+            //创建排序模板Sort
+            Sort sort = new Sort(Sort.Direction.DESC, "id");
+            //创建分页模板Pageable
+            Pageable pageable = new PageRequest(page.getPageNumber(), page.getPageSize(), sort);
+            //创建查询条件对象
+            org.springframework.data.mongodb.core.query.Query query = new org.springframework.data.mongodb.core.query.Query();
+            //条件
+            Criteria criteria = Criteria.where("orgId").is(orgId);
+            if (null != collegeId) {
+                String[] cid = collegeId.split(",");
+                Set<Long> collegeIds = new HashSet<>();
+                for (String d : cid) {
+                    collegeIds.add(Long.valueOf(d));
+                }
+                criteria.and("collegeId").in(collegeIds);
+            }
+            if (null != type) {
+                String[] td = type.split(",");
+                List tds = new ArrayList<>();
+                for (String d : td) {
+                    tds.add(Integer.valueOf(d));
+                }
+                criteria.and("education").in(tds);
+            }
+            if (null != isReport) {
+                criteria.and("isRegister").is(Integer.valueOf(isReport));
+            }
+            if (null != isPay) {
+                String[] td = type.split(",");
+                for (String d : td) {
+                    if (d.equals("1")) {
+                        criteria.and("isPay").is(1);
+                    }
+                    if (d.equals("2")) {
+                        criteria.and("isGreenChannel").is(1);
+                    }
+                }
+
+            }
+            if(!org.apache.commons.lang.StringUtils.isBlank(nj)){
+                criteria.orOperator(criteria.where("userName").regex(".*?\\" +nj+ ".*"), criteria.where("jobNum").is(nj));
+            }
+            query.addCriteria(criteria);
+            //mongoTemplate.count计算总数
+            total = mongoTemplate.count(query, StudentRegister.class);
+            // mongoTemplate.find 查询结果集
+            items = mongoTemplate.find(query.with(pageable), StudentRegister.class);
+        }catch (Exception e){
+            result.put("success", false);
+            result.put("message","获取数据异常！");
+        }
+        p.getPage().setTotalPages((int)Math.ceil(total/page.getPageSize())+1);
+        p.getPage().setPageNumber(page.getPageNumber());
+        p.getPage().setPageSize(page.getPageSize());
+        p.getPage().setTotalElements(total);
+        p.setData(items);
+        result.put("success", true);
+        result.put("data", p);
+        return result;
     }
 }
