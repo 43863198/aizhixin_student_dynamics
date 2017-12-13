9e665b3727f289549adc3a8223c506af48049c7c
王俊 <wangjun@dinglicom.com>
2017-12-13 11:36:23 +0800

    '修改表'

--- dataanalysis/src/main/java/com/aizhixin/cloud/dataanalysis/analysis/controller/TeachingEvaluateController.java
+++ dataanalysis/src/main/java/com/aizhixin/cloud/dataanalysis/analysis/controller/TeachingEvaluateController.java
@@ -14,10 +14,7 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
