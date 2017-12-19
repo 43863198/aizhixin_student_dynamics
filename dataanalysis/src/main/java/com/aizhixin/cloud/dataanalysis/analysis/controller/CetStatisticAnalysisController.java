66de3315146ed46ca09c5825a989e637dbcb97b6
wujianwei <wujianwei@aizhixin.com>
2017-12-19 20:53:15 +0800

    api 提交

--- dataanalysis/src/main/java/com/aizhixin/cloud/dataanalysis/analysis/controller/CetStatisticAnalysisController.java
+++ dataanalysis/src/main/java/com/aizhixin/cloud/dataanalysis/analysis/controller/CetStatisticAnalysisController.java
@@ -69,11 +69,13 @@ public class CetStatisticAnalysisController {
     public Map<String,Object> getCetDetail(
             @ApiParam(value = "orgId 机构id" , required = true) @RequestParam(value = "orgId", required = true) Long orgId,
             @ApiParam(value = "collegeId 机构id 注:多个时候用“，”隔开", required = false) @RequestParam(value = "collegeId", required = false) String collegeId,
+            @ApiParam(value = "teacherYear 学年" , required = true) @RequestParam(value = "teacherYear", required = true) Integer teacherYear,
+            @ApiParam(value = "semester 学期" , required = true) @RequestParam(value = "semester", required = true) Integer semester,
             @ApiParam(value = "grade 年级 注:多个时候用“，”隔开", required = false) @RequestParam(value = "grade", required = false) String grade,
             @ApiParam(value = "type 成绩类型：4:四级通过率，6:六级通过率" , required = false) @RequestParam(value = "type", required = false) Integer type,
             @ApiParam(value = "pageNumber 第几页") @RequestParam(value = "pageNumber", required = false) Integer pageNumber,
             @ApiParam(value = "pageSize 每页数据的数目") @RequestParam(value = "pageSize", required = false) Integer pageSize) {
-        return cetStatisticAnalysisService.getCetDetail(orgId, collegeId, grade, type, PageUtil.createNoErrorPageRequest(pageNumber, pageSize));
+        return cetStatisticAnalysisService.getCetDetail(orgId, collegeId,teacherYear,semester, grade, type, PageUtil.createNoErrorPageRequest(pageNumber, pageSize));
     }
 
 
