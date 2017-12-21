69d7604514dad7deb9fc239bad511c67814f2136
bly <bailiuyi@aizhixin.com>
2017-12-21 11:24:34 +0800

    导入14-16新生

--- dataanalysis/src/main/java/com/aizhixin/cloud/dataanalysis/studentRegister/service/StudentRegisterService.java
+++ dataanalysis/src/main/java/com/aizhixin/cloud/dataanalysis/studentRegister/service/StudentRegisterService.java
@@ -1,5 +1,6 @@
 package com.aizhixin.cloud.dataanalysis.studentRegister.service;
 
+import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.Map.Entry;
 
@@ -22,6 +23,7 @@ import org.springframework.web.multipart.MultipartFile;
 import com.aizhixin.cloud.dataanalysis.common.excelutil.ExcelBasedataHelper;
 import com.aizhixin.cloud.dataanalysis.common.exception.CommonException;
 import com.aizhixin.cloud.dataanalysis.common.exception.ErrorCode;
+import com.aizhixin.cloud.dataanalysis.common.util.DateUtil;
 import com.aizhixin.cloud.dataanalysis.studentRegister.domain.StudentInfoDomain;
 import com.aizhixin.cloud.dataanalysis.studentRegister.domain.StudentRegisterDomain;
 import com.aizhixin.cloud.dataanalysis.studentRegister.mongoEntity.StudentRegister;
@@ -43,110 +45,123 @@ public class StudentRegisterService {
 	@Autowired
 	private ExcelBasedataHelper basedataHelper;
 
-	public void importData(String studentInfoFile, String dataBaseFile, String importFile, Date registerDate, Long orgId) {
-		LOG.debug("star.........1");
-		//获取学生信息
+	public void importData(String studentInfoFile, String dataBaseFile, String importFile) {
+		LOG.debug("star.........getStudentInfos..14-16");
+		// 获取学生信息
 		List<StudentInfoDomain> studentInfos = basedataHelper.readStudentInfoFromInputStream(studentInfoFile);
 		if (null == studentInfos || studentInfos.size() <= 0) {
 			throw new CommonException(ErrorCode.ID_IS_REQUIRED, "没有读取到任何数据");
 		}
-		LOG.debug("star.........2");
-		//获取学生基础数据源
+		LOG.debug("star.........getDataBases");
+		// 获取学生基础数据源
 		List<StudentRegisterDomain> dataBases = basedataHelper.readStudentRegisterFromInputStream(dataBaseFile);
 		if (null == dataBases || dataBases.size() <= 0) {
 			throw new CommonException(ErrorCode.ID_IS_REQUIRED, "没有读取到任何数据");
 		}
-		LOG.debug("star.........3");
-		//获取新学生基础信息
+		LOG.debug("star.........getStudentInfos...replaceFile");
+		// 获取新学生基础信息
 		List<ImportDomain> importDomains = basedataHelper.readDataBase(importFile);
 		if (null == importDomains || importDomains.size() <= 0) {
 			throw new CommonException(ErrorCode.ID_IS_REQUIRED, "没有读取到任何数据");
 		}
-		LOG.debug("star.........4");
-		//学生基础数据存map
+		// 学生基础数据存map
 		Map<String, StudentRegisterDomain> maps = new HashMap<>();
 		for (StudentRegisterDomain data : dataBases) {
 			maps.put(data.getJobNum(), data);
 		}
-		LOG.debug("star.........5");
-		//学生信息存map
+		// 学生信息存map
 		Map<String, StudentInfoDomain> maps1 = new HashMap<>();
 		for (StudentInfoDomain data : studentInfos) {
 			maps1.put(data.getJobNum(), data);
 		}
-		LOG.debug("star.........6");
-		//新学生基础信息存map
+		// 新学生基础信息存map
 		Map<String, ImportDomain> map2 = new HashMap<>();
 		for (ImportDomain data : importDomains) {
 			map2.put(data.getName(), data);
 		}
-		LOG.debug("star.........7");
 		List<StudentRegister> stuRegisterList = new ArrayList<>();
 		int i = 0;
-		//学生数据key value
-		for (Entry<String, StudentRegisterDomain> entry : maps.entrySet()) {  
-		    //学生信息key value
-		    for (Entry<String, StudentInfoDomain> entry1 : maps1.entrySet()) {
-		    		try {
-		    			if (entry.getKey().equals(entry1.getKey())) {
-		    				StudentRegister studentRegister = new StudentRegister();
-		    				studentRegister.setOrgId(orgId);
-		    				studentRegister.setJobNum(entry.getValue().getJobNum());
-		    				if (entry.getValue().getActualRegisterDate() != null) {
-		    					Integer date = Integer.valueOf(entry.getValue().getActualRegisterDate());
-		    					if (date > 0) {
-		    						Calendar c = new GregorianCalendar(1900,0,-1);  
-		    						Date d = c.getTime();  
-		    						Date _d = DateUtils.addDays(d, date + 1);  //42605是距离1900年1月1日的天数
-		    						studentRegister.setActualRegisterDate(_d);
-		    					}
-		    				}
-		    				studentRegister.setIsRegister(entry.getValue().getIsRegister());
-		    				for (Entry<String, ImportDomain> entry2 : map2.entrySet()) {
-		    					if (entry2.getKey().equals(entry1.getValue().getClassName())) {
-		    						studentRegister.setClassId(entry2.getValue().getId());
-		    						break;
-		    					}
+		LOG.debug("star.........executeMethod");
+		// 学生信息key value
+		for (Entry<String, StudentInfoDomain> entry1 : maps1.entrySet()) {
+			// 学生数据key value
+			for (Entry<String, StudentRegisterDomain> entry : maps.entrySet()) {
+				try {
+					if (entry1.getKey().equals(entry.getKey())) {
+						StudentRegister studentRegister = new StudentRegister();
+						studentRegister.setOrgId(entry1.getValue().getOrgId());
+						studentRegister.setJobNum(entry1.getValue().getJobNum());
+						if (entry.getValue().getActualRegisterDate() != null) {
+							Integer date = Integer.valueOf(entry.getValue().getActualRegisterDate());
+							if (date > 0) {
+								Calendar c = new GregorianCalendar(1900, 0, -1);
+								Date d = c.getTime();
+								Date _d = DateUtils.addDays(d, date + 1); // 42605是距离1900年1月1日的天数
+								studentRegister.setActualRegisterDate(_d);
 							}
-		    				studentRegister.setClassName(entry1.getValue().getClassName());
-		    				studentRegister.setGrade(entry.getValue().getGrade());
-		    				for (Entry<String, ImportDomain> entry2 : map2.entrySet()) {
-		    					if (entry2.getKey().equals(entry1.getValue().getCollegeName())) {
-		    						studentRegister.setCollegeId(entry2.getValue().getId());
-		    						break;
-		    					}
+						}
+						studentRegister.setIsRegister(entry.getValue().getIsRegister());
+						for (Entry<String, ImportDomain> entry2 : map2.entrySet()) {
+							if (entry2.getKey().equals(entry1.getValue().getClassName())) {
+								studentRegister.setClassId(entry2.getValue().getId());
+								break;
 							}
-		    				studentRegister.setCollegeName(entry1.getValue().getCollegeName());
-		    				for (Entry<String, ImportDomain> entry2 : map2.entrySet()) {
-		    					if (entry2.getKey().equals(entry1.getValue().getProfessionalName())) {
-		    						studentRegister.setProfessionalId(entry2.getValue().getId());
-		    						break;
-		    					}
+						}
+						studentRegister.setClassName(entry1.getValue().getClassName());
+						studentRegister.setGrade(entry.getValue().getGrade());
+						for (Entry<String, ImportDomain> entry2 : map2.entrySet()) {
+							if (entry2.getKey().equals(entry1.getValue().getCollegeName())) {
+								studentRegister.setCollegeId(entry2.getValue().getId());
+								break;
 							}
-		    				studentRegister.setProfessionalName(entry1.getValue().getProfessionalName());
-		    				studentRegister.setSchoolYear(Integer.parseInt(entry.getValue().getSchoolYear()));
-		    				studentRegister.setUserId(entry1.getValue().getUserId());
-		    				studentRegister.setUserName(entry1.getValue().getUserName());
-		    				studentRegister.setRegisterDate(registerDate);
-		    				studentRegister.setIsPay(entry.getValue().getIsPay());
-		    				studentRegister.setIsGreenChannel(entry.getValue().getIsGreenChannel());
-		    				studentRegister.setUserPhone(entry1.getValue().getUserPhone());
-		    				stuRegisterList.add(studentRegister);
-		    			}
-		    		} catch (Exception e) {
-		    			LOG.debug("错误信息行号：" + entry.getValue().getLine() + ",  学号：" + entry.getValue().getJobNum());
-		    			LOG.debug("错误信息行号：" + entry1.getValue().getLine() + ",  学号：" + entry1.getValue().getJobNum());
-		    			e.printStackTrace();
-		    		}
+						}
+						studentRegister.setCollegeName(entry1.getValue().getCollegeName());
+						for (Entry<String, ImportDomain> entry2 : map2.entrySet()) {
+							if (entry2.getKey().equals(entry1.getValue().getProfessionalName())) {
+								studentRegister.setProfessionalId(entry2.getValue().getId());
+								break;
+							}
+						}
+						studentRegister.setProfessionalName(entry1.getValue().getProfessionalName());
+						studentRegister.setSchoolYear(Integer.parseInt(entry.getValue().getSchoolYear()));
+						studentRegister.setUserId(entry1.getValue().getUserId());
+						studentRegister.setUserName(entry1.getValue().getUserName());
+						SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+						String str = "2014-09-01";
+						String str1 = "2015-09-01";
+						String str2 = "2016-09-01";
+						if (("14").equals(entry1.getValue().getJobNum().substring(1, 3))||
+								("13").equals(entry1.getValue().getJobNum().substring(1, 3))) {
+							Date date = sdf.parse(str);
+							studentRegister.setRegisterDate(date);
+						} else if(("15").equals(entry1.getValue().getJobNum().substring(1, 3))){
+							Date date = sdf.parse(str1);
+							studentRegister.setRegisterDate(date);
+						} else{
+							Date date = sdf.parse(str2);
+							studentRegister.setRegisterDate(date);
+						}
+						studentRegister.setIsPay(entry.getValue().getIsPay());
+						studentRegister.setIsGreenChannel(entry.getValue().getIsGreenChannel());
+						studentRegister.setUserPhone(entry1.getValue().getUserPhone());
+						stuRegisterList.add(studentRegister);
+					}
+				} catch (Exception e) {
+					LOG.debug("学生报到数据表错误信息行号：" + entry.getValue().getLine() + ",  学号：" + entry.getValue().getJobNum());
+					LOG.debug("学生信息表错误信息行号：" + entry1.getValue().getLine() + ",  学号：" + entry1.getValue().getJobNum());
+					// e.printStackTrace();
+				}
 			}
-			i++;
-			if (0 == i % 1000) {
-				respository.save(stuRegisterList);
-				stuRegisterList.clear();
-			}
+//			i++;
+//			if (0 == i % 1000) {
+//				respository.save(stuRegisterList);
+//				stuRegisterList.clear();
+//			}
 		}
-		if (!stuRegisterList.isEmpty()) {
-			respository.save(stuRegisterList);
-		}
+//		if (!stuRegisterList.isEmpty()) {
+//			respository.save(stuRegisterList);
+//		}
+		respository.save(stuRegisterList);
+		System.out.println(stuRegisterList.size());
 	}
 }
