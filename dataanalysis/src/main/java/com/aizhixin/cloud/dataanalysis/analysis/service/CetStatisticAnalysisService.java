3323603ed0c3888bbf710c89231df4ab988dc85f
wujianwei <wujianwei@aizhixin.com>
2017-12-28 18:46:54 +0800

    api 提交

--- dataanalysis/src/main/java/com/aizhixin/cloud/dataanalysis/analysis/service/CetStatisticAnalysisService.java
+++ dataanalysis/src/main/java/com/aizhixin/cloud/dataanalysis/analysis/service/CetStatisticAnalysisService.java
@@ -295,15 +295,15 @@ public class CetStatisticAnalysisService {
             }
             if (null != type) {
                     if (type.equals(4)) {
-                        criteria.and("examType").is(ScoreConstant.EXAM_TYPE_CET4);
+                        criteria.and("examType").equals(ScoreConstant.EXAM_TYPE_CET4);
                         criteria.and("totalScore").gte(ScoreConstant.CET_PASS_SCORE_LINE);
                     }
                     if (type.equals(6)) {
-                        criteria.and("examType").is(ScoreConstant.EXAM_TYPE_CET6);
+                        criteria.and("examType").equals(ScoreConstant.EXAM_TYPE_CET6);
                         criteria.and("totalScore").gte(ScoreConstant.CET_PASS_SCORE_LINE);
                     }
             }else {
-                criteria.and("examType").in(ScoreConstant.EXAM_TYPE_CET4,ScoreConstant.EXAM_TYPE_CET6);
+                criteria.and("examType").ne(ScoreConstant.EXAM_TYPE_COURSE);
             }
             if(null!=nj){
                 criteria.orOperator(criteria.where("jobNum").is(nj), criteria.where("userName").regex(".*?\\" + nj + ".*"));
