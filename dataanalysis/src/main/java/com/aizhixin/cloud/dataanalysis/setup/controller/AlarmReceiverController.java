40d1c6cbd3320669afc153f5b1d2e55497a0e895
潘震 <panzhen@aizhixin.com>
2018-05-17 17:39:52 +0800

    注解修改

--- dataanalysis/src/main/java/com/aizhixin/cloud/dataanalysis/setup/controller/AlarmReceiverController.java
+++ dataanalysis/src/main/java/com/aizhixin/cloud/dataanalysis/setup/controller/AlarmReceiverController.java
@@ -100,12 +100,10 @@ public class AlarmReceiverController {
         return alarmReceiverService.getCollegeAndReceiveCount(orgId);
     }
 
-
-
     @PutMapping(value = "/msg", produces = MediaType.APPLICATION_JSON_VALUE)
     @ApiOperation(httpMethod = "PUT", value = "查询告警数据并发送告警短信", response = Void.class, notes = "查询告警数据并发送告警短信<br><br><b>@author zhen.pan</b>")
     public MessageVO setMsg(
-            @ApiParam(value = "orgId", required = true) @PathVariable Long orgId,
+            @ApiParam(value = "orgId 学校ID", required = true) @PathVariable Long orgId,
             @ApiParam(value = "teacherYear 学年") @RequestParam(value = "teacherYear", required = false) Integer teacherYear,
             @ApiParam(value = "semester 学期") @RequestParam(value = "semester", required = false) Integer semester,
             @ApiParam(value = "type 类型") @RequestParam(value = "type", required = false) String type) {
