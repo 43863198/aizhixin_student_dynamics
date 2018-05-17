93004ccdff04ccf6cb29015bbdd0b0d13c518217
潘震 <panzhen@aizhixin.com>
2018-05-17 15:06:35 +0800

    学院及接收短信人数统计接口

--- dataanalysis/src/main/java/com/aizhixin/cloud/dataanalysis/setup/respository/AlarmReceiverRespository.java
+++ dataanalysis/src/main/java/com/aizhixin/cloud/dataanalysis/setup/respository/AlarmReceiverRespository.java
@@ -1,12 +1,18 @@
 package com.aizhixin.cloud.dataanalysis.setup.respository;
 
 
+import com.aizhixin.cloud.dataanalysis.common.domain.IdCountDTO;
 import com.aizhixin.cloud.dataanalysis.setup.entity.AlarmReceiver;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface AlarmReceiverRespository extends JpaRepository<AlarmReceiver, String> {
 
     List<AlarmReceiver> findByCollegeIdAndDeleteFlag(Long collegeId, Integer deleteFlag);
+
+    @Query("select new com.aizhixin.cloud.dataanalysis.common.domain.IdCountDTO(t.collegeId, count(t.id)) from #{#entityName} t where t.deleteFlag = :deleteFlag and t.orgId=:orgId group by t.collegeId")
+    List<IdCountDTO> countByOrgAndGroupByCollegeId(@Param(value = "orgId") Long orgId, @Param(value = "orgId")Integer deleteFlag);
 }
