ce8edbeacd2a1e29975e24f01141b33dc273f0f3
潘震 <panzhen@aizhixin.com>
2018-05-18 09:35:24 +0800

    teacher拼写错误修改

--- dataanalysis/src/main/java/com/aizhixin/cloud/dataanalysis/setup/respository/AlarmReceiverRespository.java
+++ dataanalysis/src/main/java/com/aizhixin/cloud/dataanalysis/setup/respository/AlarmReceiverRespository.java
@@ -18,7 +18,7 @@ public interface AlarmReceiverRespository extends JpaRepository<AlarmReceiver, S
 
     List<AlarmReceiver> findByOrgIdAndDeleteFlag(Long orgId, Integer deleteFlag);
 
-    long countByTeacherdAndCollegeIdAndDeleteFlag(Long teacherId, Long collegeId, Integer deleteFlag);
+    long countByTeacherdIdAndCollegeIdAndDeleteFlag(Long teacherId, Long collegeId, Integer deleteFlag);
 
     long countByTeacherIdAndCollegeIdAndIdNotAndDeleteFlag(Long teacherId, Long collegeId, String id, Integer deleteFlag);
 }
