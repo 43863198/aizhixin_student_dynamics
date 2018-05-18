b033e89cb50993114eb465db3460aa4c5edf2c3c
Hushaohua <hushaohua@aizhixin.com>
2018-05-18 18:06:41 +0800

    #34 考试时间

--- dataanalysis/src/main/java/com/aizhixin/cloud/dataanalysis/stuscore/JdbcTemplate/CetScoreJdbc.java
+++ dataanalysis/src/main/java/com/aizhixin/cloud/dataanalysis/stuscore/JdbcTemplate/CetScoreJdbc.java
@@ -19,7 +19,7 @@ public class CetScoreJdbc {
     private PageJdbcUtil pageJdbcUtil;
 
     public PageData<CetScoreDomain> getByStu(Long orgId, String jobNum, Integer pageNumber, Integer pageSize) {
-        String querySql = "SELECT ts.`NAME`,tcs.JOB_NUMBER,tc.TEACHER_YEAR,tc.SEMESTER,tcs.TYPE,tcs.SCORE FROM t_cet_score tcs LEFT JOIN t_student ts ON ts.JOB_NUMBER=tcs.JOB_NUMBER AND ts.ORG_ID=tcs.ORG_ID LEFT JOIN t_school_calendar tc ON tc.ORG_ID=tcs.ORG_ID AND tcs.EXAMINATION_DATE> tc.START_TIME AND tcs.EXAMINATION_DATE< tc.END_TIME WHERE tcs.ORG_ID='" + orgId + "' AND tcs.JOB_NUMBER='" + jobNum + "'";
+        String querySql = "SELECT ts.`NAME`,tcs.JOB_NUMBER,tc.TEACHER_YEAR,tc.SEMESTER,tcs.TYPE,tcs.SCORE, tcs.EXAMINATION_DATE FROM t_cet_score tcs LEFT JOIN t_student ts ON ts.JOB_NUMBER=tcs.JOB_NUMBER AND ts.ORG_ID=tcs.ORG_ID LEFT JOIN t_school_calendar tc ON tc.ORG_ID=tcs.ORG_ID AND tcs.EXAMINATION_DATE> tc.START_TIME AND tcs.EXAMINATION_DATE< tc.END_TIME WHERE tcs.ORG_ID='" + orgId + "' AND tcs.JOB_NUMBER='" + jobNum + "'";
         String countSql = "SELECT COUNT(*) FROM t_cet_score tcs LEFT JOIN t_student ts ON ts.JOB_NUMBER=tcs.JOB_NUMBER AND ts.ORG_ID=tcs.ORG_ID LEFT JOIN t_school_calendar tc ON tc.ORG_ID=tcs.ORG_ID AND tcs.EXAMINATION_DATE> tc.START_TIME AND tcs.EXAMINATION_DATE< tc.END_TIME WHERE tcs.ORG_ID='" + orgId + "' AND tcs.JOB_NUMBER='" + jobNum + "'";
         RowMapper<CetScoreDomain> rowMapper = new RowMapper<CetScoreDomain>() {
             @Override
@@ -31,6 +31,7 @@ public class CetScoreJdbc {
                 d.setSemester(resultSet.getString("SEMESTER"));
                 d.setType(resultSet.getString("TYPE"));
                 d.setScore(resultSet.getString("SCORE"));
+                d.setDate(resultSet.getString("EXAMINATION_DATE"));
                 return d;
             }
         };
