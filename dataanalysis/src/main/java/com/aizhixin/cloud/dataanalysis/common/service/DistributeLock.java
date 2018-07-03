5e0131ecddc18158b0bdfdf0974078e0770b7852
吴建伟 <wujianwei@aizhixin.com>
2018-07-03 11:31:45 +0800

    代码提交

--- dataanalysis/src/main/java/com/aizhixin/cloud/dataanalysis/common/service/DistributeLock.java
+++ dataanalysis/src/main/java/com/aizhixin/cloud/dataanalysis/common/service/DistributeLock.java
@@ -344,21 +344,6 @@ public class DistributeLock {
         return getLock(lockPath.toString(), taskPath.toString());
     }
 
-
-    /**
-     * 生成数据的幂等锁
-     * @return  是否获取到锁
-     */
-    public boolean getWarningInfoLock(StringBuilder path) {
-        StringBuilder lockPath = new StringBuilder(zkLockPath);
-        StringBuilder taskPath = new StringBuilder(zkTaskPath);
-        lockPath.append(path);
-        taskPath.append(path);
-        return getLock(lockPath.toString(), taskPath.toString());
-    }
-
-
-
     public void cleanZookeeperTaskData() {
         delete();
     }
