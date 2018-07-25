19e78e0688234f27ca18b64be3d39facf2b6a99b
潘震 <panzhen@aizhixin.com>
2018-07-25 14:34:10 +0800

    成绩数据统计修改

--- dataanalysis/src/main/java/com/aizhixin/cloud/dataanalysis/analysis/dto/TeachingAchievementDTO.java
+++ dataanalysis/src/main/java/com/aizhixin/cloud/dataanalysis/analysis/dto/TeachingAchievementDTO.java
@@ -3,14 +3,10 @@ package com.aizhixin.cloud.dataanalysis.analysis.dto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.springframework.data.annotation.CreatedDate;
+import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.Column;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 import java.util.Date;
 
 /**
@@ -20,24 +16,25 @@ import java.util.Date;
  */
 @ApiModel(description="教学成绩")
 @Data
-@AllArgsConstructor
+@NoArgsConstructor
 public class TeachingAchievementDTO {
-    @ApiModelProperty(value = "学生人数", required = false)
+    @ApiModelProperty(value = "学生人数")
     private int studentsNum;
-    @ApiModelProperty(value = "平均GPA", required = false)
-    private double averageGPA;
-    @ApiModelProperty(value = "不及格人数", required = false)
-    private int FailNum;
-    @ApiModelProperty(value = "开设课程数量", required = false)
+    @ApiModelProperty(value = "参考学生人次")
+    private int scoreNum;
+    @ApiModelProperty(value = "必修课人次")
+    private int mustNum;
+    @ApiModelProperty(value = "必修课不及格人次")
+    private int mustFailNum;
+    @ApiModelProperty(value = "开设课程数量")
     private int coursesNum;
-    @ApiModelProperty(value = "课程平均得分", required = false)
+    @ApiModelProperty(value = "平均GPA")
+    private double averageGPA;
+    @ApiModelProperty(value = "课程平均得分")
     private double coursesAVGScore;
-    @ApiModelProperty(value = "统计时间", required = false)
-    @CreatedDate
-    @Column(name = "STATISTICAL_TIME")
+
+    @ApiModelProperty(value = "统计时间")
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
-    @Temporal(TemporalType.TIMESTAMP)
     private Date statisticalTime = new Date();
-    public TeachingAchievementDTO(){};
 }
