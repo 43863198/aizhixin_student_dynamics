5a4b97c0f0874a0051576e57a037de3373e08534
潘震 <panzhen@aizhixin.com>
2018-08-27 15:34:20 +0800

    修读预警计算

--- dataanalysis/src/main/java/com/aizhixin/cloud/dataanalysis/zb/app/respository/StudyExceptionIndexZbRespository.java
+++ dataanalysis/src/main/java/com/aizhixin/cloud/dataanalysis/zb/app/respository/StudyExceptionIndexZbRespository.java
@@ -2,7 +2,18 @@ package com.aizhixin.cloud.dataanalysis.zb.app.respository;
 
 import com.aizhixin.cloud.dataanalysis.zb.app.entity.StudyExceptionIndex;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface StudyExceptionIndexZbRespository extends JpaRepository<StudyExceptionIndex,Long>{
     void deleteByXxdmAndXnAndXqm(String xxdm, String xn, String xqm);
+    List<StudyExceptionIndex> findByXxdmAndXnAnAndXqm(String xxdm, String xn, String xqm);
+
+    @Query("SELECT max(t.xn) FROM #{#entityName} t WHERE t.xxdm = :xxdm")
+    String findLastestXnByXxdm(@Param(value = "xxdm") String xxdm);
+
+    @Query("SELECT max(t.xqm) FROM #{#entityName} t WHERE t.xxdm = :xxdm AND t.xn = :xn")
+    String findLastestXqmByXxdmAndXn(@Param(value = "xxdm") String xxdm, @Param(value = "xn") String xn);
 }
--- dataanalysis/src/main/java/com/aizhixin/cloud/dataanalysis/zb/service/StudyExceptionIndexService.java
+++ dataanalysis/src/main/java/com/aizhixin/cloud/dataanalysis/zb/service/StudyExceptionIndexService.java
@@ -1,5 +1,10 @@
 package com.aizhixin.cloud.dataanalysis.zb.service;
 
+import com.aizhixin.cloud.dataanalysis.alertinformation.entity.WarningInformation;
+import com.aizhixin.cloud.dataanalysis.common.constant.AlertTypeConstant;
+import com.aizhixin.cloud.dataanalysis.setup.entity.AlarmSettings;
+import com.aizhixin.cloud.dataanalysis.setup.entity.RuleParameter;
+import com.aizhixin.cloud.dataanalysis.setup.service.RuleParameterService;
 import com.aizhixin.cloud.dataanalysis.zb.app.entity.StudyExceptionIndex;
 import com.aizhixin.cloud.dataanalysis.zb.app.mananger.StudyExceptionIndexManager;
 import com.aizhixin.cloud.dataanalysis.zb.manager.StudyExceptionIndexZbManager;
@@ -8,9 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 /**
  * 修读异常指标计算
@@ -23,6 +26,8 @@ public class StudyExceptionIndexService {
     private StudyExceptionIndexZbManager studyExceptionIndexZbManager;
     @Autowired
     private StudyExceptionIndexManager studyExceptionIndexManager;
+    @Autowired
+    private RuleParameterService ruleParameterService;
 
     @Async
     public void calCurrentDateIndex(Long orgId) {
@@ -63,4 +68,206 @@ public class StudyExceptionIndexService {
         log.info("complete cal xxdm({}) xn({}), xq({}) studyException index.", orgId, xn, xq);
     }
 
+    /**
+     * 修读异常告警计算
+     */
+    @Async
+    public List<WarningInformation> generalAlertInfo (Long orgId, String xn, String xq, AlarmSettings as) {
+        List<WarningInformation> rsList = new ArrayList<>();
+        String zxn = null;
+        String zxq = null;
+        if (xn.indexOf("-") < 0) {//非标准学年学期转换
+            if ("秋".equals(xq)) {
+                zxn = xn = xn + "-" + (new Integer(xn) + 1);
+                zxq = "1";
+            } else if ("春".equals(xq)) {
+                zxn = xn = (new Integer(xn) - 1) + "-" + xn;
+                zxq = "2";
+            }
+        } else {
+            zxn = xn;
+            zxq = xq;
+        }
+        Set<String> paramSet = new HashSet<>();
+        if (null != as.getRuleSet()) {
+            if (as.getRuleSet().indexOf(",") != -1) {
+                String[] ruleSetIds = as.getRuleSet().split(",");
+                for (String t : ruleSetIds) {
+                    paramSet.add(t);
+                }
+            } else {
+                paramSet.add(as.getRuleSet());
+            }
+        }
+        if (!paramSet.isEmpty()) {
+            List<RuleParameter> ruleParams = ruleParameterService.getRuleParameterByIds(paramSet);
+            if (null == ruleParams || ruleParams.isEmpty()) {
+                log.info("No study exception alert params");
+                return rsList;
+            }
+
+            List<StudyExceptionIndex> studyExceptionIndexList = studyExceptionIndexManager.findByXxdmAndXnAndXqm(orgId.toString(), zxn, zxq);
+            if(studyExceptionIndexList.isEmpty()) {
+                zxn = studyExceptionIndexManager.findLastestXnByXxdm(orgId.toString());
+                if (null != xn) {
+                    zxq = studyExceptionIndexManager.findLastestXqmByXxdmAndXn(orgId.toString(), zxn);
+                    if (null != zxq) {
+                        studyExceptionIndexList = studyExceptionIndexManager.findByXxdmAndXnAndXqm(orgId.toString(), zxn, zxq);
+                    }
+                }
+            }
+            if (null != studyExceptionIndexList && !studyExceptionIndexList.isEmpty()) {
+                String tmp = null;
+                int p = -1;
+                for (StudyExceptionIndex e : studyExceptionIndexList) {
+                    WarningInformation warn = new WarningInformation ();
+                    if ("与".equals(as.getRelationship())) {
+                        createAndAlertDescAndAlertIndex(rsList, ruleParams, e, warn);
+                    } else {
+                        createOrAlertDescAndAlertIndex(rsList, ruleParams, e, warn);
+                    }
+                    String alertId = UUID.randomUUID().toString();
+                    warn.setId(alertId);
+                    warn.setName(e.getXm());
+                    warn.setJobNumber(e.getXh());
+                    warn.setCollogeCode(e.getYxsh());
+                    warn.setCollogeName(e.getYxsmc());
+                    warn.setClassCode(null);
+                    warn.setClassName(e.getBjmc());
+                    warn.setProfessionalCode(e.getZyh());
+                    warn.setProfessionalName(e.getZymc());
+                    warn.setWarningState(AlertTypeConstant.ALERT_IN_PROCESS);
+
+                    warn.setWarningTime(new Date());
+                    warn.setSemester(xq);
+                    warn.setTeacherYear(xn);
+                    warn.setOrgId(orgId);
+                }
+            }
+        }
+        return rsList;
+    }
+
+
+    private void createAndAlertDescAndAlertIndex(List<WarningInformation> rsList, List<RuleParameter> ruleParams, StudyExceptionIndex e, WarningInformation warn) {
+        StringBuilder desc = new StringBuilder();
+        StringBuilder st = new StringBuilder();
+        boolean alter = true;
+        String tmp;
+        int p;
+        for(RuleParameter r : ruleParams) {
+            if (alter && null != r.getRuledescribe() && null != r.getRightParameter()) {
+                st.append("[").append(r.getRuledescribe() + ":" + r.getRightParameter()).append("]");
+                if (r.getRuledescribe().indexOf("漏选的课程数量≥") >= 0) {
+                    if (e.getLxkcs() < new Integer(r.getRightParameter())) {
+                        alter = false;
+                        break;
+                    } else {
+                        desc.append("[").append(r.getRuledescribe()).append(e.getLxkcs()).append("]");
+                    }
+                } else if (r.getRuledescribe().indexOf("漏选的课程学分≥") >= 0) {
+                    if (e.getLxxf() < new Double(r.getRightParameter())) {
+                        alter = false;
+                        break;
+                    } else {
+                        desc.append("[").append(r.getRuledescribe()).append(e.getLxxf()).append("]");
+                    }
+                } else if (r.getRuledescribe().indexOf("课程门数占教学计划的比例≤") >= 0) {
+                    if (e.getLxkcs() * 100.0 / e.getKcs() > new Double(r.getRightParameter())) {
+                        alter = false;
+                        break;
+                    } else {
+                        tmp = (e.getLxkcs() * 100.0 / e.getKcs()) + "";
+                        p = tmp.indexOf(".");
+                        if (p > 0 && p + 2 < tmp.length()) {
+                            tmp = tmp.substring(0, p);
+                        }
+                        desc.append("[").append(r.getRuledescribe()).append(tmp).append("]");
+                    }
+                } else if (r.getRuledescribe().indexOf("课程学分占教学计划的比例≤") >= 0) {
+                    if (e.getTgxf() * 100.0 / e.getXf() > new Double(r.getRightParameter())) {
+                        alter = false;
+                        break;
+                    } else {
+                        tmp = (e.getTgxf() * 100.0 / e.getXf()) + "";
+                        p = tmp.indexOf(".");
+                        if (p > 0 && p + 2 < tmp.length()) {
+                            tmp = tmp.substring(0, p);
+                        }
+                        desc.append("[").append(r.getRuledescribe()).append(tmp).append("]");
+                    }
+                } else if (r.getRuledescribe().indexOf("考核不合格的必修课程（含集中性实践教学环节）学分≥") >= 0) {
+                    if (e.getBxbjgxf() < new Double(r.getRightParameter())) {
+                        alter = false;
+                        break;
+                    } else {
+                        desc.append("[").append(r.getRuledescribe()).append(e.getBxbjgxf()).append("]");
+
+                    }
+                }
+            }
+        }
+        if (alter) {
+            warn.setWarningCondition(desc.toString());
+            warn.setWarningStandard(st.toString());
+            rsList.add(warn);
+        }
+    }
+    private void createOrAlertDescAndAlertIndex(List<WarningInformation> rsList, List<RuleParameter> ruleParams, StudyExceptionIndex e, WarningInformation warn) {
+        StringBuilder desc = new StringBuilder();
+        StringBuilder st = new StringBuilder();
+        String tmp;
+        int p;
+        for(RuleParameter r : ruleParams) {
+            if (null != r.getRuledescribe() && null != r.getRightParameter()) {
+                st.append("[").append(r.getRuledescribe() + ":" + r.getRightParameter()).append("]");
+                if (r.getRuledescribe().indexOf("漏选的课程数量≥") >= 0) {
+                    if (e.getLxkcs() < new Integer(r.getRightParameter())) {
+                        break;
+                    } else {
+                        desc.append("[").append(r.getRuledescribe()).append(e.getLxkcs()).append("]");
+                    }
+                } else if (r.getRuledescribe().indexOf("漏选的课程学分≥") >= 0) {
+                    if (e.getLxxf() < new Double(r.getRightParameter())) {
+                        break;
+                    } else {
+                        desc.append("[").append(r.getRuledescribe()).append(e.getLxxf()).append("]");
+                    }
+                } else if (r.getRuledescribe().indexOf("课程门数占教学计划的比例≤") >= 0) {
+                    if (e.getLxkcs() * 100.0 / e.getKcs() > new Double(r.getRightParameter())) {
+                        break;
+                    } else {
+                        tmp = (e.getLxkcs() * 100.0 / e.getKcs()) + "";
+                        p = tmp.indexOf(".");
+                        if (p > 0 && p + 2 < tmp.length()) {
+                            tmp = tmp.substring(0, p);
+                        }
+                        desc.append("[").append(r.getRuledescribe()).append(tmp).append("]");
+                    }
+                } else if (r.getRuledescribe().indexOf("课程学分占教学计划的比例≤") >= 0) {
+                    if (e.getTgxf() * 100.0 / e.getXf() > new Double(r.getRightParameter())) {
+                        break;
+                    } else {
+                        tmp = (e.getTgxf() * 100.0 / e.getXf()) + "";
+                        p = tmp.indexOf(".");
+                        if (p > 0 && p + 2 < tmp.length()) {
+                            tmp = tmp.substring(0, p);
+                        }
+                        desc.append("[").append(r.getRuledescribe()).append(tmp).append("]");
+                    }
+                } else if (r.getRuledescribe().indexOf("考核不合格的必修课程（含集中性实践教学环节）学分≥") >= 0) {
+                    if (e.getBxbjgxf() < new Double(r.getRightParameter())) {
+                        break;
+                    } else {
+                        desc.append("[").append(r.getRuledescribe()).append(e.getBxbjgxf()).append("]");
+                    }
+                }
+            }
+        }
+        if (desc.length() > 0) {
+            warn.setWarningCondition(desc.toString());
+            warn.setWarningStandard(st.toString());
+            rsList.add(warn);
+        }
+    }
 }
