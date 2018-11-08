007187525277c68efbd12281eec1d8293f1dd283
潘震 <panzhen@aizhixin.com>
2018-11-08 18:12:53 +0800

    短信内容修改

--- dataanalysis/src/main/java/com/aizhixin/cloud/dataanalysis/setup/service/GenerateWarningInfoService.java
+++ dataanalysis/src/main/java/com/aizhixin/cloud/dataanalysis/setup/service/GenerateWarningInfoService.java
@@ -768,7 +768,7 @@ public class GenerateWarningInfoService {
                     break;
                     default:
             }
-            sb.append("色").append(warningTypeName).append("预警通知到你，预警内容[");
+            sb.append("色").append(warningTypeName).append("通知到你，预警内容[");
             sb.append(w.getWarningStandard());
             sb.append("]。希望你认真查找原因，努力学习，以便顺利完成学业。【桂工教务处】");
             d.setPhone(w.getPhone());
