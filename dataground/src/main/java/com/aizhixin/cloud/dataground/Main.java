0fd7c408c1076f25b874ca96e94a6f2bb5219980
潘震 <panzhen@aizhixin.com>
2018-11-21 18:00:28 +0800

    导入完善

--- dataground/src/main/java/com/aizhixin/cloud/dataground/Main.java
+++ dataground/src/main/java/com/aizhixin/cloud/dataground/Main.java
@@ -2,7 +2,9 @@ package com.aizhixin.cloud.dataground;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.Async;
 
+@Async
 @SpringBootApplication
 public class Main {
 	public static void main(String[] args) {
